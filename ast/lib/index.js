a8ff8b375fe98e25a484eff3fa5b03aa7d1cdc1a
yukon123 <yukon123@foxmail.com>
2025-01-11 14:38:22 +0800

    初始化 babe配置

---
+++ ast/lib/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(['a', 'b', 'c'].at(-1)); // 使用 Array.prototype.at
+console.log(Object.hasOwn({
+  key: 'value'
+}, 'key')); // 使用 Object.hasOwn
+
+// Babel Input: ES2015 arrow function
+[1, 2, 3].map(n => n + 1);
+
+// Babel Output: ES5 equivalent
+[1, 2, 3].map(function (n) {
+  return n + 1;
+});
+const fn = () => 1;
